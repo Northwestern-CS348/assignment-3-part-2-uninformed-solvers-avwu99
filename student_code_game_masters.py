56a6aab0dc8e173c3f586e58220fd9135f40f1ee
Austin Wu <austinwu2021@u.northwestern.edu>
2019-02-24 18:45:16 -0600

    Updated Game Master

--- student_code_game_masters.py
+++ student_code_game_masters.py
@@ -34,7 +34,46 @@ class TowerOfHanoiGame(GameMaster):
             A Tuple of Tuples that represent the game state
         """
         ### student code goes here
-        pass
+        t1 = self.kb.kb_ask(parse_input("fact: (on ?X peg1)"))
+        t1List = list()
+
+        if not t1:
+            pass
+        else:
+            for i in t1:
+                app = str(i)
+                t1List.append(int(app[-1]))
+
+        t1List.sort()
+        p1 = tuple(t1List)
+
+        t2 = self.kb.kb_ask(parse_input("fact: (on ?X peg2)"))
+        t2List = list()
+
+        if not t2:
+            pass
+        else:
+            for i in t2:
+                app1 = str(i)
+                t2List.append(int(app1[-1]))
+
+        t2List.sort()
+        p2 = tuple(t2List)
+
+        t3 = self.kb.kb_ask(parse_input("fact: (on ?X peg3)"))
+        t3List = list()
+
+        if not t3:
+            pass
+        else:
+            for i in t3:
+                app2 = str(i)
+                t3List.append(int(app2[-1]))
+
+        t3List.sort()
+        p3 = tuple(t3List)
+
+        return (p1, p2, p3)
 
     def makeMove(self, movable_statement):
         """
@@ -53,7 +92,50 @@ class TowerOfHanoiGame(GameMaster):
             None
         """
         ### Student code goes here
-        pass
+        terms = movable_statement.terms
+        moveDisk = terms[0].__str__()
+        oldPeg = terms[1].__str__()
+        newPeg = terms[2].__str__()
+
+        # Retract old 'on' fact
+        fact1 = Fact(Statement(['on', moveDisk, oldPeg]))
+        self.kb.kb_retract(fact1)
+
+        # Assert new 'on' fact
+        fact2 = Fact(Statement(['on', moveDisk, newPeg]))
+        self.kb.kb_assert(fact2)
+
+        # Find the disk under the moveDisk
+        answer = self.kb.kb_ask(Fact(Statement(["onTopOf", moveDisk, '?X'])))
+        if answer:  # If a disk is found under moveDisk
+            diskUnder = answer[0].bindings_dict['?X']
+            fact3 = Fact(Statement(['onTopOf', moveDisk, diskUnder]))
+            self.kb.kb_retract(fact3)
+            fact7 = Fact(Statement(['top', diskUnder, oldPeg]))
+        else: # If there is no disk under the moveDisk
+            fact3 = Fact(Statement(['empty', oldPeg]))
+            self.kb.kb_assert(fact3)
+
+        # If the new peg was empty, retract that fact
+        check = parse_input("fact: (empty %s)" %newPeg)
+        if check:
+            fact4 = Fact(Statement(['empty', newPeg]))
+            self.kb.kb_retract(fact4)
+
+        # Retract old fact of moveDisk being on the oldPeg and assert new fact of moveDisk on newPeg
+        fact5 = Fact(Statement(['top', moveDisk, oldPeg]))
+        self.kb.kb_retract(fact5)
+        fact6 = Fact(Statement(['top', moveDisk, newPeg]))
+        self.kb.kb_assert(fact6)
+
+        # Retract old onTop fact of disk currently on the newPeg and assert new fact of moveDisk onTop of newPeg
+        answer = self.kb.kb_ask(Fact(Statement(['onTopOf', '?X', newPeg])))
+        if answer:  # If there is currently a disk on top of the newPeg
+            diskReplace = answer[0].bindings_dict['?X']
+            fact8 = Fact(Statement(['onTop', diskReplace, newPeg]))
+            self.kb.kb_retract(fact8)
+            fact9 = Fact(Statement(['onTopOf', moveDisk, diskReplace]))
+            self.kb.kb_assert(fact9)
 
     def reverseMove(self, movable_statement):
         """
@@ -100,7 +182,47 @@ class Puzzle8Game(GameMaster):
             A Tuple of Tuples that represent the game state
         """
         ### Student code goes here
-        pass
+        tile_dict = {"tile1": 1, "tile2": 2, "tile3": 3, "tile4": 4, "tile5": 5, "tile6": 6, "tile7": 7, "tile8": 8,
+                     "empty": -1}
+        x_dict = {"pos1": 0, "pos2": 1, "pos3": 2}
+
+        row1 = self.kb.kb_ask(Fact(Statement(["coordinate", '?tile', '?X', "pos1"])))
+        rowList1= [0,0,0]
+        for i in row1:
+            val = i.bindings_dict['?tile']
+            x = i.bindings_dict['?X']
+            translateTile = tile_dict[val]  # tile value
+            translateX = x_dict[x]
+            rowList1[translateX] = translateTile
+
+        t1 = tuple(rowList1)
+
+        row2 = self.kb.kb_ask(Fact(Statement(["coordinate", '?tile', '?X', "pos2"])))
+        rowList2 = [0,0,0]
+        for j in row2:
+            val = j.bindings_dict['?tile']
+            x = j.bindings_dict['?X']
+            translateTile = tile_dict[val]
+            translateX = x_dict[x]
+            rowList2[translateX] = translateTile
+
+        t2 = tuple(rowList2)
+
+        row3 = self.kb.kb_ask(Fact(Statement(["coordinate", '?tile', '?X', "pos3"])))
+        rowList3 = [0,0,0]
+        for k in row3:
+            val = k.bindings_dict['?tile']
+            x = k.bindings_dict['?X']
+            translateTile = tile_dict[val]
+            translateX = x_dict[x]
+            rowList3[translateX] = translateTile
+
+        t3 = tuple(rowList3)
+
+        print(row3)
+        print(t1, t2, t3)
+        return (t1, t2, t3)
+
 
     def makeMove(self, movable_statement):
         """
@@ -119,7 +241,29 @@ class Puzzle8Game(GameMaster):
             None
         """
         ### Student code goes here
-        pass
+        # Essentially were swapping positions of the tile in the movable statement with the empty tile
+        terms = movable_statement.terms
+        tile = terms[0].__str__()
+        oldX = terms[1].__str__()
+        oldY = terms[2].__str__()
+        newX = terms[3].__str__()
+        newY = terms[4].__str__()
+
+        # Retract old coordinates of the moving tile
+        fact1 = Fact(Statement(['coordinate', tile, oldX, oldY]))
+
+        # Assert new coordinates of the moving tile
+        fact2 = Fact(Statement(['coordinate', tile, newX, newY]))
+
+        # Do the same with the empty tile
+        fact3 = Fact(Statement(['coordinate', 'empty', newX, newY]))
+
+        fact4 = Fact(Statement(['coordinate', 'empty', oldX, oldY]))
+
+        self.kb.kb_retract(fact1)
+        self.kb.kb_retract(fact3)
+        self.kb.kb_assert(fact2)
+        self.kb.kb_assert(fact4)
 
     def reverseMove(self, movable_statement):
         """
